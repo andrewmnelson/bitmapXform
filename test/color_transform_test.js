f8567f0e006de87abc30b27f1c6b3c5964cb7ca1
Andrew Nelson <andrewnelson@earthlink.net>
2015-09-07 22:03:10 -0700

    working modular xform

--- test/color_transform_test.js
+++ test/color_transform_test.js
@@ -5,13 +5,13 @@ var colorXform = require('../color_transform');
 var expect = require('chai').expect;
 
 describe('colorXform', function() {
-  it('should handle known transform codes', function() {
+  it('handles known transform codes', function() {
     expect(colorXform("G").xformName).to.eql('Grayscale');
     expect(colorXform("R").xformName).to.eql('Rotate');
     expect(colorXform("I").xformName).to.eql('Invert');
     expect(colorXform("N").xformName).to.eql('No Transform');
   });
-  it('should handle unknown transform codes', function() {
+  it('errors unknown transform codes', function() {
     expect(colorXform('@').xformName).to.eql('');
     expect(colorXform('@').xformFunction).to.eql(null);
   });
